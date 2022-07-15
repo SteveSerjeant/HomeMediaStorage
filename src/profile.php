f680d3a54082e284ef143d8d436e9873d9895bc8
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-07-15 16:34:51 +0100

    ommit of progress to date up to inclusion of add new user code.

--- src/profile.php
+++ src/profile.php
@@ -36,7 +36,7 @@ if (mysqli_connect_errno()) {
 <nav class="navtop">
     <div>
         <h1>Home Media Storage</h1>
-        <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+        <a href="mainPage.php"><i class="fas fa-arrow-left"></i>Back</a>
         <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
     </div>
 </nav>
